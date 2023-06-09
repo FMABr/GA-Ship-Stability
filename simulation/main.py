d155fc374c193cde0c68a7077187f3cbfeb9933c
FMABr <felipemuamorim@gmail.com>
2023-06-09 18:12:47 -0400

    First demonstration

--- simulation/main.py
+++ simulation/main.py
@@ -1,8 +1,23 @@
-from ursina import *
 import ui
+import loading
+
+from ursina import *
 
 app = Ursina()
 
-ui.Controls("Controles", lambda x: print(next(x)))
+boat = loading.FerryBoat()
+boat.rotation_y = 270
+
+camera = EditorCamera()
+camera.position = 500, 300, 0
+camera.rotation_x = 30
+camera.rotation_y = -90
+
+
+def add_cargo(location):
+    boat.load(loading.Cargo(), next(location))
+
+
+ui.Controls("Controles", add_cargo)
 
 app.run()
